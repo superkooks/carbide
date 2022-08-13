a9e687c8733d75a466030ad87871e587643d4d59
SuperKooks <superkooks@superkooks.com>
2022-08-13 21:48:44 +1000

    Init webapp

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="body1">Hello world</div>
+
+  <span class="material-symbols-rounded"> cancel </span>
+</template>
+
+<style>
+@import "./assets/theme.css";
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0");
+
+body {
+  color: var(--md-on-background);
+  background: var(--md-surface1);
+}
+</style>
